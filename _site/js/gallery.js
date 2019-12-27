887fb8ec521bfff93345077081be1a5ab7c5b548
greglas <gregoracius@yahoo.com>
2019-12-27 06:02:45 +0200

    Finish image Gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,29 @@
+
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery(){
-  alert(document.querySelector("h1").textContent)
+  let thumbnails=document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage=document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail){
+    thumbnail.addEventListener("click", function(){
+      // code to set clicked image as main image
+      let newImageSrc=thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+    //removes .current class form the 1st image and adds it to any image clicked
+    document.querySelector(".current").classList.remove("current");
+    thumbnail.parentNode.classList.add("current");
+    //updates info of the image
+    let galleryInfo = document.querySelector("#gallery-info");
+    let title       = galleryInfo.querySelector(".title");
+    let description = galleryInfo.querySelector(".description");
+
+    title.innerHTML       = thumbnail.dataset.title;
+    description.innerHTML = thumbnail.dataset.description;
+    })
+  })
 }
